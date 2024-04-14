50e0007b287cfe85c7cdfa33259f5aa4d95e7636
AlonaLeeds <113086090+AlonaLeeds@users.noreply.github.com>
2024-04-14 18:26:37 +0000

    Deploying to gh-pages from @ ArtemGoiT/Go-it@99ecf0725141e01715c1c0b2e8a5d18378d1755a 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,9 +1,8 @@
-import{a as i}from"./assets/vendor-34f890c2.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))c(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&c(s)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function c(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();const l=i.create({baseURL:"https://dummyjson.com"});async function u(){return await l.get("/products")}const a=o=>o.map(({id:r,title:n,description:c,brand:e,price:t,images:s})=>`    <li data-id="${r}">
-      <img src="${s[0]}" alt="${n}">
-      <h3>Brand:${e}</h3>
-      <h4>model:${n}</h4>
-      <p>description:${c}</p>
-      <p>price:${t}</p>
-      
-    </li>`).join(),d=document.querySelector("#allProducts");u().then(({data:{products:o}})=>{console.log(o);const r=a(o);d.insertAdjacentHTML("beforebegin",r)});
+import{a as d}from"./assets/vendor-34f890c2.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const c of t.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&n(c)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const u=d.create({baseURL:"https://dummyjson.com"});async function l(o){return await u.get(`/products/${o}`)}const a=({id:o,title:r,description:s,brand:n,price:e,images:t})=>`<div data-id="${o}">
+      <img src="${t[0]}" alt="${r}">
+      <h3>Brand:${n}</h3>
+      <h4>model:${r}</h4>
+      <p>description:${s}</p>
+      <p>price:${e}</p>
+    </div>`,i=document.querySelector("#singleProductForm"),f=document.querySelector("#singleProduct");i.addEventListener("submit",m);function m(o){o.preventDefault();const r=o.target.elements.id.value;l(r).then(({data:s})=>{const n=a(s);f.innerHTML=n,i.reset()})}
 //# sourceMappingURL=commonHelpers.js.map
